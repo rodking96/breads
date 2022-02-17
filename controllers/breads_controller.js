7738e91ce957b899777cad18eac6b6bff3f218e4
Rodrigo Reyes <rodrigoreyes96@gmail.com>
2022-02-16 19:02:09 -0500

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,29 +1,35 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
-
 // INDEX
-breads.get('/', (req, res) => {
-  res.send(Bread)
-})
+// breads.get('/', (req, res) => {
+//   res.send(Bread)
+// })
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
-    res.send('404')
+    res.render('404')
   }
 })
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.render('Index', {
+  res.render('index', {
     breads: Bread
   })
 // res.send(Bread)
+})
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
 })
 
 module.exports = breads
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -10,8 +11,8 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-// MIDDLEWARE
 app.use(express.static('public'))
+app.use(methodOverride('_method'))
 
 // ROUTES
 app.get('/', (req, res) => {
